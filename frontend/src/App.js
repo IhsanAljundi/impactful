7e77bced601ebabc41966a305e6b2693eaf4943e
Marcellino Chris O'Vara <marcellinocovara@gmail.com>
2023-11-21 00:00:50 +0700

    feat: setup tailwind and router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,21 @@
+import { Route, Routes } from "react-router-dom";
+
+import { Home } from "./modules/home/Home";
+import { Register } from "./modules/register/Register";
+import { Login } from "./modules/login/Login";
+import { Profile } from "./modules/profile/Profile";
+
 function App() {
-  return <div></div>;
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+
+      <Route path="/register" element={<Register />} />
+      <Route path="/login" element={<Login />} />
+
+      <Route path="/profile" element={<Profile />} />
+    </Routes>
+  );
 }
 
 export default App;
