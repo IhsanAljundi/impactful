9ba12ad1a38b144684e34cb568b15d319e488f53
Marcellino Chris O'Vara <marcellinocovara@gmail.com>
2023-11-23 13:51:05 +0700

    feat: implement auth

---
+++ backend/models/User.js
@@ -0,0 +1,25 @@
+"use strict";
+
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+
+const UserSchema = new mongoose.Schema({
+  username: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  role: {
+    type: String,
+    enum: ["individual", "organization"],
+    default: "individual",
+  },
+});
+
+UserSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  this.password = await bcrypt.hash(this.password, 10);
+});
+
+UserSchema.methods.comparePassword = async function (password) {
+  return bcrypt.compare(password, this.password);
+};
+
+module.exports = mongoose.model("User", UserSchema);
