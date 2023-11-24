f3648c17c70795fbffc29a1974223944102c90a0
Marcellino Chris O'Vara <marcellinocovara@gmail.com>
2023-11-24 23:09:15 +0700

    feat: add icons

---
+++ frontend/src/icons/index.js
@@ -0,0 +1,7 @@
+export { ReactComponent as ChevronIcon } from "./chevron.svg";
+export { ReactComponent as EmailIcon } from "./email.svg";
+export { ReactComponent as LoadingIcon } from "./loading.svg";
+export { ReactComponent as LockIcon } from "./lock.svg";
+export { ReactComponent as MessageIcon } from "./message.svg";
+export { ReactComponent as StarIcon } from "./star.svg";
+export { ReactComponent as UserIcon } from "./user.svg";
