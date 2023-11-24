d3f75055918077ab7c73649f57426f7209247b7e
Marcellino Chris O'Vara <marcellinocovara@gmail.com>
2023-11-24 12:49:02 +0700

    feat: add color

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -7,6 +7,21 @@ module.exports = {
         montserrat: ["Montserrat"],
       },
     },
+    colors: {
+      blue: "#52C3FF",
+      gold: "#FFCB57",
+      peach: "#FFDFD6",
+      pink: "#FC77A0",
+      stone: "#172239",
+      turqoise: "#5AE2E2",
+      gray: {
+        100: "#F9F9F9",
+        200: "#EAEAEA",
+        300: "#D5D4D4",
+        700: "#6D6D78",
+        900: "#161616",
+      },
+    },
   },
   plugins: [],
 };
