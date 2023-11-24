3169587e5fc802c57890ba65939aafab781993ff
Marcellino Chris O'Vara <marcellinocovara@gmail.com>
2023-11-24 23:08:02 +0700

    feat: add username to user model

--- backend/models/User.js
+++ backend/models/User.js
@@ -4,6 +4,7 @@ const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
 
 const UserSchema = new mongoose.Schema({
+  email: { type: String, required: true, unique: true },
   username: { type: String, required: true, unique: true },
   password: { type: String, required: true },
   role: {
