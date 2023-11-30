011eba59a637f1782e367dbe2e082e26fc0e9f74
IhsanAljundi <106752410+IhsanAljundi@users.noreply.github.com>
2023-12-01 02:02:23 +0700

    Delete backend/models/contribution.js

--- backend/models/contribution.js
+++
@@ -1,9 +0,0 @@
-// models/contribution.js
-const mongoose = require('mongoose');
-
-const contributionSchema = new mongoose.Schema({
-    type: { type: String, required: true }, // 'volunteer' or 'donation'
-    // other fields as needed
-});
-
-module.exports = mongoose.model('Contribution', contributionSchema);
