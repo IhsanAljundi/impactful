ab0d83b4e63b8c488080b430d91b53d185f76a90
IhsanAljundi <106752410+IhsanAljundi@users.noreply.github.com>
2023-12-01 02:01:48 +0700

    Update BE

--- backend/controllers/contributionController.js
+++ backend/controllers/contributionController.js
@@ -1,22 +1,53 @@
-// controllers/contributionController.js
-const Contribution = require('../models/contribution');
+const Volunteer = require('../models/volunteer');
+const Donation = require('../models/donation');
 
-exports.createContribution = async (req, res) => {
-    try {
-        const { type } = req.body;
-        const contribution = new Contribution({ type });
-        await contribution.save();
-        res.status(201).json({ success: true, message: 'Contribution created successfully' });
-    } catch (error) {
-        res.status(500).json({ success: false, error: error.message });
-    }
-};
+module.exports = {
+  volunteer: async (req, res) => {
+    const { name, email, reason, availability, campaign } = req.body;
 
-exports.getListOfContributions = async (req, res) => {
     try {
-        const contributions = await Contribution.find();
-        res.status(200).json({ success: true, data: contributions });
+      // Simpan data volunteer ke database
+      const newVolunteer = new Volunteer({ name, email, reason, availability, campaign });
+      await newVolunteer.save();
+
+      return res.status(201).json({ success: true, message: 'Volunteer created successfully' });
     } catch (error) {
-        res.status(500).json({ success: false, error: error.message });
+      console.error(error);
+      return res.status(500).json({ success: false, message: 'Internal Server Error' });
     }
+  },
+
+  donation: async (req, res) => {
+    const { donationAmount, email, payment, campaign } = req.body;
+
+    try {
+      // Simpan data donation ke database
+      const newDonation = new Donation({ donationAmount, email, payment, campaign });
+      await newDonation.save();
+
+      return res.status(201).json({ success: true, message: 'Donation created successfully' });
+    } catch (error) {
+      console.error(error);
+      return res.status(500).json({ success: false, message: 'Internal Server Error' });
+    }
+  },
+
+  getListContributions: async (req, res) => {
+    const userId = req.user.id; // Ambil ID pengguna dari data otentikasi (asumsi otentikasi sudah ditangani sebelumnya)
+
+    try {
+      // Ambil semua kontribusi (volunteer dan donation) dari pengguna dengan ID tertentu
+      const volunteerContributions = await Volunteer.find({ userId }).populate('campaign');
+      const donationContributions = await Donation.find({ userId }).populate('campaign');
+
+      return res.status(200).json({
+        success: true,
+        volunteerContributions,
+        donationContributions,
+      });
+    } catch (error) {
+      console.error(error);
+      return res.status(500).json({ success: false, message: 'Internal Server Error' });
+    }
+  },
 };
