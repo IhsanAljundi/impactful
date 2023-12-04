9c5d91eca90b372c84d524148504dd7b8c8a55ad
IhsanAljundi <106752410+IhsanAljundi@users.noreply.github.com>
2023-12-05 00:38:28 +0700

    Update BE : List Contribution Campaign Card

--- backend/routes/contributionRoutes.js
+++ backend/routes/contributionRoutes.js
@@ -6,5 +6,6 @@ const contributionController = require('../controllers/contributionController');
 router.post('/volunteer', contributionController.volunteer);
 router.post('/donation', contributionController.donation);
 router.get('/contributions', contributionController.getListContributions);
+router.get('/followed-campaigns', contributionController.getListFollowedCampaigns);
 
 module.exports = router;
